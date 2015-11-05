f3003736533d366a2ed2653ebfc333dc16819180
Trent Newton <trent99878@gmail.com>
2015-11-05 23:24:59 +1000

    merged changes to new layout

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,41 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(function() {
+	$(".right-off-canvas-toggle, .exit-off-canvas").click(function() {
+    	$(".top-bar").toggleClass("top-bar-close");
+		$(".middle-bar").toggleClass("middle-bar-close");
+		$(".bottom-bar").toggleClass("bottom-bar-close");
+  	});
+});
+
+
+// scroll to sections
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+new vUnit({
+  CSSMap: {
+    // The selector (VUnit will create rules ranging from .selector1 to .selector100)
+    '.vh_height': {
+      // The CSS property (any CSS property that accepts px as units)
+      property: 'height',
+      // What to base the value on (vh, vw, vmin or vmax)
+      reference: 'vh'
+    }
+  },
+}).init(); // call the public init() method
