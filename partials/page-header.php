e59bed973d8f43aeb3c39521c800d26fa1d6d5ae
Trent Newton <trent99878@gmail.com>
2016-01-21 12:11:41 +1000

    mobile adjustments

--- partials/page-header.php
+++ partials/page-header.php
@@ -1,13 +1,15 @@
 <?php include("side-navigation.php"); ?>
 <div class="off-canvas-content gallery-page-header-background" data-off-canvas-content>
-	<header class="top-navigation">
-		<div class="top-navigation-left">
-			<a class="logo-link" title="Home" href="/riddelphotographers/">
-				<img class="main-logo" src="/riddelphotographers/assets/images/logo.png" alt="Riddel Photographers">
-	    	</a>
-		</div>
-		<div class="top-navigation-right">
-			<?php include("navigation.php"); ?>
+	<header class="main-page-header">
+		<div class="top-navigation">
+			<div class="top-navigation-left">
+				<a class="logo-link" title="Home" href="/riddelphotographers/">
+					<img class="main-logo" src="/riddelphotographers/assets/images/logo.png" alt="Riddel Photographers">
+		    	</a>
+			</div>
+			<div class="top-navigation-right">
+				<?php include("navigation.php"); ?>
+			</div>
 		</div>
 		<button class="hamburger" type="button" data-toggle="offCanvasRight">
 			<span class="bar"></span>
