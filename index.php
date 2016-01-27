3a4d8fc657a81a3126d58607b206bbdd5ea85b3d
Trent Newton <trent99878@gmail.com>
2016-01-27 21:43:13 +1000

    Delete index.php

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php 
-	
-	// Set this so the system will know they are using index.php?/this/that type URLs
-	$rewrite = false;
-	require 'app' . DIRECTORY_SEPARATOR . 'site' . DIRECTORY_SEPARATOR . 'site.php';
