66d7d8994454c71d720984090bae2e55593bdc79
Trent Newton <trent99878@gmail.com>
2016-03-29 14:50:03 +1000

    removed space

--- storage/themes/riddelphotographers/assets/js/app.js
+++ storage/themes/riddelphotographers/assets/js/app.js
@@ -38,7 +38,7 @@ new vUnit({
       timeout: false,
       timeoutCountdown: 5000,
       onLoadEvent: true,
-      browser: [ 'animation-duration', '-webkit-animation-duration'],
+      browser: ['animation-duration', '-webkit-animation-duration'],
       // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
       // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
       overlay : false,
