dff3ce67218ba1ff940a0327444288f57f77745b
Trent Newton <trent99878@gmail.com>
2016-07-19 09:21:55 +1000

    added fix for aspect ratio detection [deploy:production]

--- storage/themes/riddelphotographers/assets/js/app.js
+++ storage/themes/riddelphotographers/assets/js/app.js
@@ -94,12 +94,12 @@ new vUnit({
 
     // detect portrait photos and add class
 
-    $('.image-items-list .image-item > img[data-presets*="tiny,40,60"]').parent().addClass('half');
-    $('.image-items-list .image-item.0.667').addClass('half');
-    $('.image-items-list .image-item > img[alt*="portrait"]').addClass('half');
-    $('.image-items-list .image-item > img[alt*="landscape"][data-presets*="tiny,40,60"]').parent().removeClass('half');
-    $('.image-items-list .image-item > img[alt*="landscape"][data-presets*="tiny,24,60"]').parent().removeClass('half');
-    $('.image-items-list .image-item.0.667 > img[alt*="landscape"]').parent().removeClass('half');
+    // $('.image-items-list .image-item > img[data-presets*="tiny,40,60"]').parent().addClass('half');
+    $('.image-items-list .image-item.0\\.667').addClass('half');
+    $('.image-items-list .image-item > img[alt*="portrait"]').parent().addClass('half');
+    // $('.image-items-list .image-item > img[alt*="landscape"][data-presets*="tiny,40,60"]').parent().removeClass('half');
+    $('.image-items-list .image-item > img[alt*="landscape"]').parent().removeClass('half');
+    $('.image-items-list .image-item.0\\.667 > img[alt*="landscape"]').parent().removeClass('half');
 
   });
 
