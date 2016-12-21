6d5b7994f1cb45810a554a2db300ebfdaa3a4a99
Trent Newton <trent99878@gmail.com>
2016-12-21 18:14:18 +1000

    fixed js issues

--- storage/themes/riddelphotographers/assets/js/background-clip-text-polyfill.js
+++ storage/themes/riddelphotographers/assets/js/background-clip-text-polyfill.js
@@ -32,26 +32,26 @@ Element.prototype.backgroundClipPolyfill = function () {
       el = this;
 
   function hasBackgroundClip() {
-    return b.style.webkitBackgroundClip != undefined;
-  };
-  
+    return b.style.webkitBackgroundClip !== undefined;
+  }
+
   function addAttributes(el, attributes) {
     for (var key in attributes) {
       el.setAttribute(key, attributes[key]);
     }
   }
-  
+
   function createSvgElement(tagname) {
     return d.createElementNS('http://www.w3.org/2000/svg', tagname);
   }
-  
+
   function createSVG() {
     var a = arguments[0],
         svg = createSvgElement('svg'),
         pattern = createSvgElement('pattern'),
         image = createSvgElement('image'),
         text = createSvgElement('text');
-    
+
     // Add attributes to elements
     addAttributes(pattern, {
       'id' : a.id,
@@ -59,13 +59,13 @@ Element.prototype.backgroundClipPolyfill = function () {
       'width' : a.width,
       'height' : a.height
     });
-    
+
     addAttributes(image, {
       'width' : a.width,
       'height' : a.height
     });
     image.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', a.url);
-    
+
     addAttributes(text, {
       'x' : '50%',
       'y' : '50%',
@@ -73,20 +73,20 @@ Element.prototype.backgroundClipPolyfill = function () {
       'style' : 'fill:url(#' + a.id + ');',
       'dy' : '.15em'
     });
-    
+
     // Set text
     text.textContent = a.text;
-      
+
     // Add elements to pattern
     pattern.appendChild(image);
-      
+
     // Add elements to SVG
     svg.appendChild(pattern);
     svg.appendChild(text);
-    
+
     return svg;
-  };
-  
+  }
+
   /*
    * Replace the element if background-clip
    * is not available.
@@ -102,9 +102,9 @@ Element.prototype.backgroundClipPolyfill = function () {
         'height' : this.height,
         'text' : el.textContent
       });
-      
+
       el.parentNode.replaceChild(svg, el);
-    }
+    };
     img.src = a.patternURL;
   }
 };
