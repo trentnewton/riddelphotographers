eec82c45cc761155941e8e0754e2ec1135850d0b
Trent Newton <trent99878@gmail.com>
2017-01-18 13:14:14 +1000

    improved redirect

--- storage/themes/riddelphotographers/assets/js/app.js
+++ storage/themes/riddelphotographers/assets/js/app.js
@@ -24,10 +24,10 @@ new vUnit({
 
   $(document).ready(function () {
 
-    if(document.location.href.indexOf('paulakern') > -1) {
-        // indexOf will return the position of the first occurence of this string in the url
-        // or -1 it it's not there.
-        document.location.href = 'http://www.riddelphotographers.com.au';
+    // redirect if paulakern.com.au
+
+    if (/http:\/\/(www\.)?paulakern\.com.au/.test(document.referrer)) {
+       window.location = 'http://www.riddelphotographers.com.au';
     }
 
     $('.animsition').animsition({
