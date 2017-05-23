e9843defeed2a67ad9f104f6c11049c8b8ed81f5
Trent Newton <trent99878@gmail.com>
2017-05-23 22:18:47 +1000

    added space

--- storage/themes/riddelphotographers/assets/js/app.js
+++ storage/themes/riddelphotographers/assets/js/app.js
@@ -116,7 +116,7 @@
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
-        $('html,body').animate({
+        $('html, body').animate({
           scrollTop: target.offset().top
         }, 1000);
         return false;
